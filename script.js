b40d5ddb7ec884add9f4cb9fdddf6c30d97dfda4
milenapoteriko <106338663+milenapoteriko@users.noreply.github.com>
2024-10-09 18:24:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,33 +7,48 @@ const perguntas = [ //serve para abrir lista de perguntas
     {//abre o objeto das perguntas
         enunciado: "O que é Inteligencia Artificial?",
         alternativas: [
-            {texto:"Uma simulação da inteligencia humana", afirmação: "afirmação da alternativa"},
-            {texto:"Um novo tipo de planta", afirmação: "afirmacão da alternativa"}]
+            {texto:"Uma simulação da inteligencia humana",
+            afirmação: "A IA é uma simulção de inteligencia humana"},
+
+            {texto:"Um novo tipo de planta",
+             afirmação: "A IA é um novo tipo de planta"}]
     },
     {
         enunciado: "A IA na saude é usada para",
         alternativas: [
-            {texto:"Substituir todos os médicos ", afirmação: "afirmacão da alternativa"},
-            {texto:"Diagnosticar doenças mais rapidamente",afirmação: "afirmacão da alternativa"}]
+            {texto:"Substituir todos os médicos ",
+             afirmação: "A IA é utilizada na saúde para substituir todos os médicos"},
+
+            {texto:"Diagnosticar doenças mais rapidamente",
+            afirmação: "A IA é utilizada na saúde para diagnosticar doenças mais rapidamente "}]
     },
     {
-        enunciado: "Assistentes virtuais como Siri ou alexa ajudam com:",
+        enunciado: "Assistentes virtuais como Siri ou Alexa ajudam com:",
         alternativas: [
-            {texto:"Programação avançada de software",afirmação: "afirmacão da alternativa"},
-            {texto:"Tarefas do dia a dia e controle de dispositivos",afirmação: "afirmacão da alternativa"}]   
+            {texto:"Programação avançada de software",
+            afirmação: "Assistentes virtuais como Siri e Alexa ajudam com progrmação avancada de software"},
+
+            {texto:"Tarefas do dia a dia e controle de dispositivos",
+            afirmação: "Assistentes virtuais como Siri ou Alexa ajudam com tarefas do dia a dia e com controle de dispositivos"}]   
     },
     {
         enunciado: "a evolução da IA pode resultar em:",
         alternativas: [
-            {texto:"Maior automação nas industrias",afirmação: "afirmacão da alternativa"},
-            {texto:"Menos tecnologia no cotidiano das pessoas",afirmação: "afirmacão da alternativa"}]
+            {texto:"Maior automação nas industrias",
+            afirmação: "A evolução da IA pode resultar em uma maior automação nas indústrias"},
+
+            {texto:"Menos tecnologia no cotidiano das pessoas",
+            afirmação: "A evolução da IA pode resultar em menos tecnologia para as pessoas"}]
 
     },
     {
         enunciado: "O aprendizado de maquina é",
         alternativas: [
-            {texto:"Uma maneira de ensinar maquinas",afirmação: "afirmacão da alternativa"},
-            {texto:"Um tipo de jogo",afirmação: "afirmacão da alternativa"}]
+            {texto:"Uma maneira de ensinar maquinas",
+            afirmação: "O aprendizado de máquinas é uma maneira de ensinar máquinas"},
+
+            {texto:"Um tipo de jogo",
+            afirmação: "O aprendizado de máquinas é um tipo de jogo"}]
 
     }
 ]
@@ -42,8 +57,13 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta(){
+    if(posicao >=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 function mostraAlternativas(){
@@ -56,9 +76,14 @@ function mostraAlternativas(){
 }
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmação;
-    historiaFinal = afirmacoes;
+    historiaFinal += afirmacoes +" ";
     posicao++;
     mostraPergunta();
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Voce acredita que a IA é..."
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
